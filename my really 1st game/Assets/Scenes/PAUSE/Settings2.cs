c517a9f294f7f38b19e62e1d7d4b451a1e9e32cb
server-ok <68389045+server-ok@users.noreply.github.com>
2023-05-26 19:40:58 +0300

    init

---
+++ my really 1st game/Assets/Scenes/PAUSE/Settings2.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Settings2 : MonoBehaviour
+{
+
+    Resolution[] resolutions;
+
+    void Start()
+    {
+
+        List<string> options = new List<string>();
+        resolutions = Screen.resolutions;
+        int currentResolutionIndex = 0;
+
+        LoadSettings(currentResolutionIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SaveSettings()
+    {
+        PlayerPrefs.SetInt("FullscreenPreference", System.Convert.ToInt32(Screen.fullScreen));
+    }
+
+    public void LoadSettings(int currentResolutionIndex)
+    {
+        if (PlayerPrefs.HasKey("FullscreenPreference"))
+            Screen.fullScreen = System.Convert.ToBoolean(PlayerPrefs.GetInt("FullscreenPreference"));
+        else
+            Screen.fullScreen = true;
+    }
+
+}
---
+++ my really 1st game/Assets/scripts/Scenes.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenes : MonoBehaviour
+{
+    public int sceneNumber;
+
+    public void Transition()
+    {
+        SceneManager.LoadScene(sceneNumber);
+        Time.timeScale = 1f;
+    }
+}
