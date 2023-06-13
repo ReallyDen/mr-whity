2e17e8bc922d6010acb8c67c1365baf4882fbe5a
serverok <68389045+server-ok@users.noreply.github.com>
2023-06-13 19:06:10 +0300

    paused or not that's the question
    step 1

--- MrWhity/Assets/Scenes/PAUSE/pause.cs
+++ MrWhity/Assets/Scenes/PAUSE/pause.cs
@@ -6,17 +6,20 @@ public class pause : MonoBehaviour
 {
     public GameObject panel;
     public KeyCode spaceKey;
+    public bool paused = false;
 
     public void Pause()
     {
         panel.SetActive(true);
         Time.timeScale = 0;
+        paused = true;
     }
 
     public void Continue()
     {
         panel.SetActive(false);
         Time.timeScale = 1;
+        paused = false;
     }
 
     public void ExitGame()
