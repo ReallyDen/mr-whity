42b10e2913a470dc84ad2aefe3ccf0663bad21b1
serverok <68389045+server-ok@users.noreply.github.com>
2023-06-16 23:08:18 +0300

    может быть фикс коробок
    мда

--- MrWhity/Assets/scripts/DragAndDrop.cs
+++ MrWhity/Assets/scripts/DragAndDrop.cs
@@ -5,12 +5,14 @@ using UnityEngine.EventSystems;
 public class DragAndDrop : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
     private RectTransform rectTransform;
+    private Canvas canvas;
     private Image image;
 
     private void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
         image = GetComponent<Image>();
+        canvas = GetComponentInParent<Canvas>();
     }
 
     public void OnBeginDrag(PointerEventData eventData)
@@ -20,7 +22,7 @@ public class DragAndDrop : MonoBehaviour, IBeginDragHandler, IEndDragHandler, ID
 
     public void OnDrag(PointerEventData eventData)
     {
-        rectTransform.anchoredPosition += eventData.delta;
+        rectTransform.anchoredPosition += eventData.delta/canvas.scaleFactor;
     }
 
     public void OnEndDrag(PointerEventData eventData)
