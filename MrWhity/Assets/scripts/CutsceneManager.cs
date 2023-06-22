8ccab5e5e1ea1c294563483df035f5b8190f83b4
serverok <68389045+server-ok@users.noreply.github.com>
2023-06-22 22:20:37 +0300

    скипать катсцену (вау!)

---
+++ MrWhity/Assets/scripts/CutsceneManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CutsceneManager : MonoBehaviour
+{
+    public PlayableDirector currentTimeline;
+    void Start()
+    { }
+    void Awake()
+    {
+        currentTimeline = GetComponent<PlayableDirector>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            currentTimeline.time = currentTimeline.duration; ; //скипаем
+        }
+    }
+}
