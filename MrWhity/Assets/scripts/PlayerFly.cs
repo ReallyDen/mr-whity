e6e09b5dfc4001e423fd76d8655af030e8769f48
ReallyDen <den080208abr@gmail.com>
2023-07-07 17:59:23 +0300

    Дрочился  - ниче не получилось, че-та сделал

---
+++ MrWhity/Assets/scripts/PlayerFly.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFly : MonoBehaviour
+{
+
+    [Header("Player Flying Settings")]
+    private Vector2 targetPos;
+    public float Yincrement;
+    public float Xincrement;
+    public float speed;
+
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        // Move Y
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y + Yincrement);
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y - Yincrement);
+        }
+
+        // Move X
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            targetPos = new Vector2(transform.position.x + Xincrement, transform.position.y);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            targetPos = new Vector2(transform.position.x - Xincrement, transform.position.y);
+        }
+    }
+}
