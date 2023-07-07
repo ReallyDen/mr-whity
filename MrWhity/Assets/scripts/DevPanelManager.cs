3b25c495a5fd90034816a201848e193d73e47d0f
ReallyDen <den080208abr@gmail.com>
2023-07-07 15:34:33 +0300

    DevMenu remade

---
+++ MrWhity/Assets/scripts/DevPanelManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevPanelManager : MonoBehaviour
+{
+    public GameObject panel;
+    // public GameObject oldPanel;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            panel.SetActive(true);
+          //  oldPanel.SetActive(false);
+        }
+    }
+}
---
+++ MrWhity/Assets/scripts/OldDragAndDrop.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class OldDragAndDrop : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    private RectTransform rectTransform;
+    private Image image;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        image = GetComponent<Image>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        image.raycastTarget = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += eventData.delta;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        image.raycastTarget = true;
+    }
+}
